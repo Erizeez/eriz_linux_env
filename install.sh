77dc4e99c764be71aff1745a5dec0721faccbcf3
Erizeez <48181303+Erizeez@users.noreply.github.com>
2023-08-15 10:02:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "当前用户密码将被修改为 666666"
 echo -e "666666\n666666" | sudo passwd $(whoami)
 
 # 取消 root 用户输入密码的要求
-sudo sed -i 's/^root\tALL=(ALL:ALL) ALL$/root\tALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+sudo sed -i 's/^sudo\tALL=(ALL:ALL) ALL$/sudo\tALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
 
 # 输出当前的可用 IP 地址
 ip=$(curl -s ifconfig.me)
