198d95e536e2f6749ad74797f6e63a9757b0e185
Erizeez <48181303+Erizeez@users.noreply.github.com>
2023-08-15 09:47:09 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 安装 vim 和 git 软件包
+sudo apt-get update
+sudo apt-get install -y vim git net-tools openssh-server
+
+# 修改当前用户密码为 666666
+echo "当前用户密码将被修改为 666666"
+echo -e "666666\n666666" | sudo passwd $(whoami)
+
+# 取消 root 用户输入密码的要求
+sudo sed -i 's/^root\tALL=(ALL:ALL) ALL$/root\tALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+
+# 输出当前的可用 IP 地址
+ip=$(curl -s ifconfig.me)
+echo "当前的可用 IP 地址是：$ip"
+
+echo "脚本执行完毕！"
